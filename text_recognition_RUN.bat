3ba5eb6580c4c74c1aca7a795bbd9072d2222981
Stefanache Ion <ion.stefanache@yahoo.com>
2020-03-19 15:54:10 +0200

    Update text_recognition_RUN.bat

--- text_recognition_RUN.bat
+++ text_recognition_RUN.bat
@@ -12,8 +12,20 @@ rem pip install pillow
 rem pip install pytesseract
 rem pip install imutils
 
-pip install -r requirements.txt
+rem pip install -r requirements.txt
+rem pause
+
+python text_recognition.py --east frozen_east_text_detection.pb --image images/example_01.jpg 
 pause
 
-python text_recognition.py --east frozen_east_text_detection.pb --image images/example_05.jpg --padding 0.05
+python text_recognition.py --east frozen_east_text_detection.pb --image images/example_02.jpg 
+pause
+
+python text_recognition.py --east frozen_east_text_detection.pb --image images/example_03.jpg 
+pause
+
+python text_recognition.py --east frozen_east_text_detection.pb --image images/example_04.jpg --padding 0.05
+pause
+
+python text_recognition.py --east frozen_east_text_detection.pb --image images/example_05.jpg --padding 0.25
 pause
